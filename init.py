6d47af18f53518807861ac4bab8d1fb690d20d83
CESF_athos <sousa.athos@escola.pr.gov.br>
2025-08-27 11:30:51 -0300

    Add files via upload

---
+++ init.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('init.html')
+
+@app.route('/documentaçao')
+def documentation():
+    return redirect(render_template('documentation.html'))
+
+@app.route('/videos')
+def videos_page():
+    return render_template('videos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
